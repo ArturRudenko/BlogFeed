2068aeaaa4cddd887e508db289db110b28177470
Artur Rudenko <sparksbridge@gmail.com>
2022-07-19 16:26:30 +0400

    extract post previews list to a variable

--- src/pages/feed/Feed.tsx
+++ src/pages/feed/Feed.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useMemo} from "react";
 import styles from './feed.module.scss'
 import {useGetPostsQuery} from "../../store/blog/blog.api";
 import {DEFAULT_USER_ID} from "../../utils/constants";
@@ -7,10 +7,14 @@ import {PostPreview} from "../../components/post-preview";
 const Feed: FC = () => {
   const {data: posts} = useGetPostsQuery(DEFAULT_USER_ID)
 
+  const postsList = useMemo(() =>
+    posts?.map((post) => <PostPreview key={post.id} post={post}/>
+  ), [posts])
+
   return (
     <div className={styles.container}>
       <section className={styles.feed}>
-        {posts?.map((post) => <PostPreview key={post.id} post={post}/>)}
+        {postsList}
       </section>
     </div>
   )
